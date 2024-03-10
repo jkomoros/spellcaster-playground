37d658e6e651091036ea0f905b8ca9ab3f3a48ba
Alex Komoroske <jkomoros@gmail.com>
2024-03-10 15:00:26 -0700

    Get spellcaster sample code working, from example.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello world');
+import {signal} from 'spellcaster/spellcaster.js';
+import {tags, text} from 'spellcaster/hyperscript.js'
+
+const {div, button} = tags
+
+const Counter = (): HTMLElement => {
+  const [count, setCount] = signal(0)
+
+  return div(
+    {className: 'counter'},
+    [
+      div({className: 'counter-text'}, text(count)),
+      button(
+        {
+          className: 'counter-button',
+          onclick: () => setCount(count() + 1)
+        },
+        text('Increment')
+      )
+    ]
+  )
+}
+
+document.body.append(Counter())
